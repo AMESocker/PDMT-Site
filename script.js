b833d5b74288a156e1f13194d328fbd88e80a452
AMESocker <aesocher@yahoo.com>
2024-03-06 15:25:17 -0800

    added event listener and element selectors

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello')
+const navHome = document.getElementById('navHome');
+const homeEle = document.getElementById('home')
+
+navHome.addEventListener('click',()=>{
+  console.log('Hello')
+  homeEle.style.display = 'contents'
+})
