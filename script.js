7801bc4baef820c97c8b14fd48163941725ebe8c
AMESocker <aesocher@yahoo.com>
2024-03-11 17:53:52 -0700

    Added Quote page styling

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-const navHome = document.getElementById('navHome');
+const navHome = document.getElementById('nav-home');
 const homeEle = document.getElementById('home');
-const navQuote = document.getElementById('navQuote');
+const navQuote = document.getElementById('nav-quote');
 const quoteEle = document.getElementById('quote');
-const navDes = document.getElementById('navDes')
-const desEle = document.getElementById('destinations')
+const navDes = document.getElementById('nav-des');
+const desEle = document.getElementById('destinations');
+const navOnline = document.getElementById('nav-online');
+const onlineEle = document.getElementById('online')
 
+//----Event Listeners----
 homeEle.style.display = 'contents'
 quoteEle.style.display = 'none'
 desEle.style.display = 'none'
-navHome.addEventListener('click',()=>{
+onlineEle.style.display = 'none'
+navHome.addEventListener('click', () => {
   console.log('Home')
   homeEle.style.display = 'contents'
   quoteEle.style.display = 'none'
   desEle.style.display = 'none'
+  onlineEle.style.display = 'none'
 })
-navQuote.addEventListener('click',()=>{
+navQuote.addEventListener('click', () => {
   console.log('Quote')
   homeEle.style.display = 'none'
   quoteEle.style.display = 'contents'
   desEle.style.display = 'none'
+  onlineEle.style.display = 'none'
 })
-navDes.addEventListener('click',()=>{
+navDes.addEventListener('click', () => {
   console.log('Destinations')
   homeEle.style.display = 'none'
   quoteEle.style.display = 'none'
   desEle.style.display = 'contents'
+  onlineEle.style.display = 'none'
+})
+navOnline.addEventListener('click', () => {
+  console.log('Destinations')
+  homeEle.style.display = 'none'
+  quoteEle.style.display = 'none'
+  desEle.style.display = 'none'
+  onlineEle.style.display = 'contents'
 })
