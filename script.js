d2827b9547cb3a2b7e84dc9c3fbc27c4483ab3fd
AMESocker <aesocher@yahoo.com>
2024-04-29 11:44:15 -0700

    added navigation active elements

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const onlineEle = document.getElementById('online');
 const navDisDes = document.getElementById('nav-dis-des');
 const disDesEle = document.getElementById('disney-destinations');
 const scLogoEle = document.getElementById('scLogo')
+const navTeam = document.getElementById('nav-team')
+const teamEle = document.getElementById('team')
 const cruiseSection = document.getElementById('cruise-section');
 const cruiseRadioYes = document.getElementById('cruise-radio-yes');
 const cruiseRadioNo = document.getElementById('cruise-radio-no');
@@ -19,50 +21,75 @@ quoteEle.style.display = 'none'
 desEle.style.display = 'none'
 onlineEle.style.display = 'none'
 cruiseSection.style.display = 'none'
-disDesEle.style.display = 'none'
+// disDesEle.style.display = 'none'
 
 //----Event Listeners----
 navHome.addEventListener('click', () => {
+  navHome.classList.add('active')
+  navQuote.classList.remove('active')
+  navDes.classList.remove('active')
+  navTeam.classList.remove('active')
   console.log('Home')
   homeEle.style.display = 'contents'
   quoteEle.style.display = 'none'
   desEle.style.display = 'none'
   onlineEle.style.display = 'none'
-  disDesEle.style.display = 'none'
+  // disDesEle.style.display = 'none'
 });
 navQuote.addEventListener('click', () => {
+  navQuote.classList.add('active')
+  navHome.classList.remove('active')
+  navDes.classList.remove('active')
+  navTeam.classList.remove('active')
   console.log('Quote')
   homeEle.style.display = 'none'
   quoteEle.style.display = 'contents'
   desEle.style.display = 'none'
   onlineEle.style.display = 'none'
-  disDesEle.style.display = 'none'
+  // disDesEle.style.display = 'none'
 });
 navDes.addEventListener('click', () => {
+  navDes.classList.add('active')
+  navQuote.classList.remove('active')
+  navHome.classList.remove('active')
+  navTeam.classList.remove('active')
   console.log('Destinations')
   homeEle.style.display = 'none'
   quoteEle.style.display = 'none'
   desEle.style.display = 'contents'
   onlineEle.style.display = 'none'
-  disDesEle.style.display = 'none'
+  // disDesEle.style.display = 'none'
 });
-navOnline.addEventListener('click', () => {
-  console.log('Online')
-  homeEle.style.display = 'none'
-  quoteEle.style.display = 'none'
-  desEle.style.display = 'none'
-  onlineEle.style.display = 'contents'
-  disDesEle.style.display = 'none'
-});
-navDisDes.addEventListener('click',()=>{
+navTeam.addEventListener('click',() =>{
+  navTeam.classList.add('active')
+  navDes.classList.remove('active')
+  navQuote.classList.remove('active')
+  navHome.classList.remove('active')
+  console.log('Meet our Team')
+  teamEle.style.display = 'contents'
   homeEle.style.display = 'none'
   quoteEle.style.display = 'none'
   desEle.style.display = 'none'
   onlineEle.style.display = 'none'
-  disDesEle.style.display = 'contents'
-  dclEle.style.display = 'none'
-  wdwEle.style.display = 'none'
 })
+// navOnline.addEventListener('click', () => {
+//   console.log('Online')
+//   homeEle.style.display = 'none'
+//   quoteEle.style.display = 'none'
+//   desEle.style.display = 'none'
+//   onlineEle.style.display = 'contents'
+//   disDesEle.style.display = 'none'
+// });
+// navDisDes.addEventListener('click',()=>{
+//   navDisDes.classList.toggle('active')
+//   homeEle.style.display = 'none'
+//   quoteEle.style.display = 'none'
+//   desEle.style.display = 'none'
+//   onlineEle.style.display = 'none'
+//   disDesEle.style.display = 'contents'
+//   dclEle.style.display = 'none'
+//   wdwEle.style.display = 'none'
+// })
 //----dynamic cruise display----
 cruiseRadioYes.addEventListener('click',()=>{
   if(cruiseRadioYes){
@@ -81,12 +108,12 @@ const dclEle = document.getElementById('dcl');
 const wdwNav = document.getElementById('nav-wdw');
 const wdwEle = document.getElementById('wdw')
 
-dclNav.addEventListener('click',()=>{
-  dclEle.style.display = 'contents'
-  wdwEle.style.display = 'none'
-})
+// dclNav.addEventListener('click',()=>{
+//   dclEle.style.display = 'contents'
+//   wdwEle.style.display = 'none'
+// })
 
-wdwNav.addEventListener('click',()=>{
-  dclEle.style.display = 'none'
-  wdwEle.style.display = 'contents'
-})
+// wdwNav.addEventListener('click',()=>{
+//   dclEle.style.display = 'none'
+//   wdwEle.style.display = 'contents'
+// })
